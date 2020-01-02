6725ddf78595bc2df647022859f43073c3ca7a05
Keiichi Takahashi <bitwalk@gmail.com>
2020-01-02 20:33:24 +0900

    modified gtk_combobox.py added gtk_iconview.py

---
+++ gtk_iconview.py
@@ -0,0 +1,30 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository.GdkPixbuf import Pixbuf
+
+icons = ["edit-cut", "edit-paste", "edit-copy"]
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="アイコンビュー")
+        self.set_default_size(200, 200)
+
+        liststore = Gtk.ListStore(Pixbuf, str)
+        iconview = Gtk.IconView.new()
+        iconview.set_model(liststore)
+        iconview.set_pixbuf_column(0)
+        iconview.set_text_column(1)
+
+        for icon in icons:
+            pixbuf = Gtk.IconTheme.get_default().load_icon(icon, 64, 0)
+            liststore.append([pixbuf, icon])
+
+        self.add(iconview)
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
