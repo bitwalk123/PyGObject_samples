47d735df01982ae64b46afd1a54c40159fa7a215
Keiichi Takahashi <bitwalk@gmail.com>
2020-01-11 09:15:13 +0900

    changed class variable location

--- calculator.py
+++ calculator.py
@@ -3,26 +3,25 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-num_info = [["０", 0, 5, 1, 1],
-            ["１", 0, 4, 1, 1],
-            ["２", 1, 4, 1, 1],
-            ["３", 2, 4, 1, 1],
-            ["４", 0, 3, 1, 1],
-            ["５", 1, 3, 1, 1],
-            ["６", 2, 3, 1, 1],
-            ["７", 0, 2, 1, 1],
-            ["８", 1, 2, 1, 1],
-            ["９", 2, 2, 1, 1]]
-dot_info = ["・", 1, 5, 1, 1]
-func_info = [["±", 2, 1, 1, 1],
-             ["＋", 3, 4, 1, 2],
-             ["ー", 3, 3, 1, 1],
-             ["×", 3, 2, 1, 1],
-             ["÷", 3, 1, 1, 1],
-             ["＝", 2, 5, 1, 1]]
-
 
 class Calculator(Gtk.Window):
+    num_info = [["０", 0, 5, 1, 1],
+                ["１", 0, 4, 1, 1],
+                ["２", 1, 4, 1, 1],
+                ["３", 2, 4, 1, 1],
+                ["４", 0, 3, 1, 1],
+                ["５", 1, 3, 1, 1],
+                ["６", 2, 3, 1, 1],
+                ["７", 0, 2, 1, 1],
+                ["８", 1, 2, 1, 1],
+                ["９", 2, 2, 1, 1]]
+    dot_info = ["・", 1, 5, 1, 1]
+    func_info = [["±", 2, 1, 1, 1],
+                 ["＋", 3, 4, 1, 2],
+                 ["ー", 3, 3, 1, 1],
+                 ["×", 3, 2, 1, 1],
+                 ["÷", 3, 1, 1, 1],
+                 ["＝", 2, 5, 1, 1]]
 
     def __init__(self):
         Gtk.Window.__init__(self, title="電卓")
@@ -54,18 +53,18 @@ class Calculator(Gtk.Window):
         self.set_value_to_display("0.")
 
     def num_keys(self):
-        for info in num_info:
+        for info in self.num_info:
             but = Gtk.Button(label=info[0])
             but.connect("clicked", self.on_num_button_clicked)
             self.grid.attach(but, info[1], info[2], info[3], info[4])
 
     def dot_key(self):
-        but = Gtk.Button(label=dot_info[0])
+        but = Gtk.Button(label=self.dot_info[0])
         but.connect("clicked", self.on_dot_button_clicked)
-        self.grid.attach(but, dot_info[1], dot_info[2], dot_info[3], dot_info[4])
+        self.grid.attach(but, self.dot_info[1], self.dot_info[2], self.dot_info[3], self.dot_info[4])
 
     def func_keys(self):
-        for info in func_info:
+        for info in self.func_info:
             but = Gtk.Button(label=info[0])
             but.connect("clicked", self.on_func_button_clicked)
             self.grid.attach(but, info[1], info[2], info[3], info[4])
