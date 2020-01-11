89a4ccd432f6020477401b3217b9fc94258214f1
Keiichi Takahashi <bitwalk@gmail.com>
2020-01-11 21:40:57 +0900

    implemented minimum error handles.

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ from gi.repository import Gtk
 # CSS
 CALCULATOR_CSS = '''
 button {
-    padding: 5px 25px;
+    padding: 5px 30px;
     margin: 1px;
 }
 #Base {
@@ -45,7 +45,6 @@ class Calculator(Gtk.Window):
                 ["７", 0, 2, 1, 1],
                 ["８", 1, 2, 1, 1],
                 ["９", 2, 2, 1, 1]]
-    dot_info = ["・", 1, 5, 1, 1]
     ope_info = [["＋", 3, 4, 1, 2],
                 ["ー", 3, 3, 1, 1],
                 ["×", 3, 2, 1, 1],
@@ -53,9 +52,15 @@ class Calculator(Gtk.Window):
     func_info = [["±", 2, 1, 1, 1],
                  ["√", 1, 1, 1, 1]]
 
+    # max length
+    max_chars = 16
+
     # operation flag
-    dot_entered = False
-    ope_entered = False
+    flag_dot = False
+    flag_operation = False
+    flag_error = False
+
+    # register for calculation
     reg = queue.Queue()
 
     # regular expression
@@ -92,7 +97,12 @@ class Calculator(Gtk.Window):
             return value * -1
         # square root
         if text == "√":
-            return math.sqrt(value)
+            try:
+                return math.sqrt(value)
+            except Exception as e:
+                self.flag_error = True
+                return e
+
 
     def get_operator(self, text):
         if text == "＋":
@@ -118,10 +128,15 @@ class Calculator(Gtk.Window):
         context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
         self.grid.attach(self.ent, 0, 0, 4, 1)
 
-        self.key_dot()
         self.keys_func()
         self.keys_ope()
         self.keys_num()
+
+        but_D = Gtk.Button(name="Key", label="・")
+        but_D.connect("clicked", self.on_dot_button_clicked)
+        context = but_D.get_style_context()
+        context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+        self.grid.attach(but_D, 1, 5, 1, 1)
 
         but_E = Gtk.Button(name="Key", label="＝")  # Equal
         but_E.connect("clicked", self.on_equal)
@@ -137,13 +152,6 @@ class Calculator(Gtk.Window):
 
         # initialize
         self.on_clear(but_C)
-
-    def key_dot(self):
-        but = Gtk.Button(name="Key", label=self.dot_info[0])
-        but.connect("clicked", self.on_dot_button_clicked)
-        context = but.get_style_context()
-        context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
-        self.grid.attach(but, self.dot_info[1], self.dot_info[2], self.dot_info[3], self.dot_info[4])
 
     def keys_func(self):
         for info in self.func_info:
@@ -174,30 +182,45 @@ class Calculator(Gtk.Window):
         self.set_display("0.")
 
         # clear flag
-        self.dot_entered = False
-        self.ope_entered = False
+        self.flag_dot = False
+        self.flag_operation = False
+        self.flag_error = False
 
     def on_dot_button_clicked(self, button):
+        if self.flag_error:
+            return
+
         # flag
-        self.dot_entered = True
+        self.flag_dot = True
 
     def on_equal(self, button):
+        if self.flag_error:
+            return
+
         expr = ""
         while not self.reg.empty():
             expr += self.reg.get()
 
         expr += self.ent.get_text()
 
-        result = eval(expr)
+        try:
+            result = eval(expr)
+        except Exception as e:
+            result = e
+            self.flag_error = True
+
         disp_new = self.get_display_string(result)
 
         # display
         self.set_display(disp_new)
 
         # flag
-        self.ope_entered = True
+        self.flag_operation = True
 
     def on_func_button_clicked(self, button):
+        if self.flag_error:
+            return
+
         # get current value displayed
         value_current = float(self.ent.get_text())
 
@@ -211,9 +234,12 @@ class Calculator(Gtk.Window):
         self.set_display(disp_new)
 
         # flag
-        self.ope_entered = True
+        self.flag_operation = True
 
     def on_ope_button_clicked(self, button):
+        if self.flag_error:
+            return
+
         # get current string displayed
         disp_current = self.ent.get_text()
         self.reg.put(disp_current)
@@ -223,27 +249,35 @@ class Calculator(Gtk.Window):
         self.reg.put(self.get_operator(text))
 
         # flag
-        self.ope_entered = True
+        self.flag_operation = True
 
     def on_num_button_clicked(self, button):
-        text = button.get_label()
-        text_ascii = self.zenkaku_to_hankaku(text)
+        if self.flag_error:
+            return
 
         # get current string displayed
         disp_current = self.ent.get_text()
 
+        # get string from key label
+        text = button.get_label()
+        text_ascii = self.zenkaku_to_hankaku(text)
+
         # update string to display
-        if self.ope_entered:
+        if self.flag_operation:
             disp_new = text_ascii + "."
-            self.ope_entered = False
+            self.flag_operation = False
         else:
             if disp_current == "0.":
-                if self.dot_entered:
+                if self.flag_dot:
                     disp_new = disp_current + text_ascii
                 else:
                     disp_new = text_ascii + "."
             else:
-                if self.dot_entered:
+                # check charcter length (digit)
+                if len(disp_current) > self.max_chars:
+                    return
+
+                if self.flag_dot:
                     disp_new = disp_current + text_ascii
                 else:
                     result = self.re1.match(disp_current)
