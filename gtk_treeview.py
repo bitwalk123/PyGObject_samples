9fb886c6d8b419e22ca4cbe4118794e5f57d7a2d
Keiichi Takahashi <bitwalk@gmail.com>
2020-01-25 21:30:53 +0900

    modifyied tree items

--- gtk_treeview.py
+++ gtk_treeview.py
@@ -12,30 +12,31 @@ class MyWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="ツリービュー")
         self.set_default_size(200, 300)
         vbox = Gtk.VBox()
+        self.add(vbox)
 
-        treeview = Gtk.TreeView(model=self.create_store())
-        tvcolumn = Gtk.TreeViewColumn('GUI ツールキット')
-        treeview.append_column(tvcolumn)
+        tree = Gtk.TreeView(model=self.create_store())
+        column = Gtk.TreeViewColumn(title='GUI ツールキット')
+        tree.append_column(column)
 
         cell = Gtk.CellRendererText()
-        tvcolumn.pack_start(cell, True)
-        tvcolumn.add_attribute(cell, 'text', 0)
-        vbox.add(treeview)
+        column.pack_start(cell, True)
+        column.add_attribute(cell, 'text', 0)
 
-        self.add(vbox)
+        vbox.add(tree)
 
     def create_store(self):
         store = Gtk.TreeStore(str)
 
-        row1 = store.append(None, ['JAVA'])
+        row1 = store.append(None, ['Java'])
         store.append(row1, ['AWT'])
+        store.append(row1, ['SWT'])
         store.append(row1, ['Swing'])
         store.append(row1, ['JavaFX'])
 
         row2 = store.append(None, ['Python'])
         store.append(row2, ['PyQt'])
         store.append(row2, ['wxPython'])
-        store.append(row2, ['PyGTK'])
+        store.append(row2, ['PyGObject'])
 
         return store
 
