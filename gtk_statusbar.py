962e5d5f52c84fab47965b5a810e7f111905e64b
Keiichi Takahashi <bitwalk@gmail.com>
2020-02-18 20:20:42 +0900

    modified gtk_statusbar.py

--- gtk_statusbar.py
+++ gtk_statusbar.py
@@ -1,76 +1,38 @@
-import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 
-class MyWindow(Gtk.ApplicationWindow):
-    # a window
+class MyWindow(Gtk.Window):
 
-    def __init__(self, app):
-        Gtk.Window.__init__(self, title="StatusBar Example", application=app)
-        self.set_default_size(200, 100)
+    def __init__(self):
+        Gtk.Window.__init__(self, title="ステータスバー")
+        self.set_default_size(0, 0)
 
-        # a label
-        label = Gtk.Label(label="Press any key or ")
+        lab = Gtk.Label(label="　何かキーを押下、または　")
+        but = Gtk.Button(label="ここをクリック")
+        but.connect("clicked", self.button_clicked)
 
-        # a button
-        button = Gtk.Button(label="click me.")
-        # connected to a callback
-        button.connect("clicked", self.button_clicked_cb)
+        self.status = Gtk.Statusbar()
+        self.context_id = self.status.get_context_id("example")
+        self.status.push(self.context_id, "何かするのを待っています…")
 
-        # the statusbar
-        self.statusbar = Gtk.Statusbar()
-        # its context_id - not shown in the UI but needed to uniquely identify
-        # the source of a message
-        self.context_id = self.statusbar.get_context_id("example")
-        # we push a message onto the statusbar's stack
-        self.statusbar.push(
-            self.context_id, "Waiting for you to do something...")
-
-        # a grid to attach the widgets
         grid = Gtk.Grid()
-        grid.set_column_spacing(5)
-        grid.set_column_homogeneous(True)
-        grid.set_row_homogeneous(True)
-        grid.attach(label, 0, 0, 1, 1)
-        grid.attach_next_to(button, label, Gtk.PositionType.RIGHT, 1, 1)
-        grid.attach(self.statusbar, 0, 1, 2, 1)
-
-        # add the grid to the window
+        grid.attach(lab, 0, 0, 1, 1)
+        grid.attach_next_to(but, lab, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach(self.status, 0, 1, 2, 1)
         self.add(grid)
 
-    # callback function for the button clicked
-    # if the button is clicked the event is signaled to the statusbar
-    # onto which we push a new status
-    def button_clicked_cb(self, button):
-        self.statusbar.push(self.context_id, "You clicked the button.")
+    def button_clicked(self, button):
+        self.status.push(self.context_id, "ボタンをクリックしました。")
 
-    # event handler
     def do_key_press_event(self, event):
-        # any signal from the keyboard is signaled to the statusbar
-        # onto which we push a new status with the symbolic name
-        # of the key pressed
-        self.statusbar.push(self.context_id, Gdk.keyval_name(event.keyval) +
-                            " key was pressed.")
-        # stop the signal emission
+        self.status.push(self.context_id, Gdk.keyval_name(event.keyval) + " キーを押しました。")
         return True
 
 
-class MyApplication(Gtk.Application):
-
-    def __init__(self):
-        Gtk.Application.__init__(self)
-
-    def do_activate(self):
-        win = MyWindow(self)
-        win.show_all()
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
