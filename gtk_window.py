4bf9fe675dc38e617208e87c95f81ceb3e29fc75
Keiichi Takahashi <bitwalk@gmail.com>
2020-02-20 20:54:36 +0900

    modified gtk_window.py

--- gtk_window.py
+++ gtk_window.py
@@ -4,16 +4,29 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
-class ChildWindow(Gtk.Window):
+class SubWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="サブ")
+        self.set_default_size(0, 0)
 
-        button = Gtk.Button(label="このボタンをクリックするとウィンドウを閉じます。")
-        button.connect("clicked", self.on_button_clicked)
-        self.add(button)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(box)
+
+        self.ent = Gtk.Entry()
+        self.ent.set_text("ここは入力欄です。")
+        self.ent.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "accessories-text-editor")
+
+        but = Gtk.Button(label="閉じる")
+        but.connect("clicked", self.on_button_clicked)
+
+        box.pack_start(self.ent, True, True, 0)
+        box.pack_start(but, False, False, 0)
+
+    def get_text(self):
+        return self.ent.get_text()
 
     def on_button_clicked(self, widget):
-        print("ウィンドウを閉じます。")
+        print("サブウィンドウを閉じます。")
         self.close()
 
 
@@ -22,19 +35,23 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="メイン")
 
-        button = Gtk.Button(label="このボタンをクリックすると、子ウィンドウが開きます。")
+        button = Gtk.Button(label="このボタンをクリックすると\nサブウィンドウが開きます。")
         button.connect("clicked", self.on_button_clicked)
         self.add(button)
 
     def on_button_clicked(self, widget):
-        print("クリックされました。")
+        print("サブウィンドウを開きます。")
 
-        child = ChildWindow()
-        child.set_transient_for(parent=self)
-        child.set_modal(True)
-        child.set_deletable(False)
-        child.connect("destroy", Gtk.Widget.destroy)
-        child.show_all()
+        subwin = SubWindow()
+        subwin.set_transient_for(parent=self)
+        subwin.set_modal(True)
+        subwin.set_deletable(False)
+        subwin.connect("destroy", self.get_text_from_child)
+        subwin.show_all()
+
+    def get_text_from_child(self, object):
+        print("入力されたエントリの文字列：", object.get_text())
+        del object
 
 
 win = MyWindow()
