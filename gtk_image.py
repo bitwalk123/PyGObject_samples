ddb319d5f1b7053f6a34b12f18781d111ff1510d
Keiichi Takahashi <bitwalk@gmail.com>
2020-03-08 19:59:25 +0900

    modified gtk_image.py

--- gtk_image.py
+++ gtk_image.py
@@ -7,14 +7,14 @@ from gi.repository import Gtk
 class MyWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="ラベル")
+        Gtk.Window.__init__(self, title="イメージ")
         self.set_default_size(0, 0)
 
+        img_list = [Gtk.STOCK_HOME, Gtk.STOCK_COPY, Gtk.STOCK_CUT, Gtk.STOCK_PASTE, Gtk.STOCK_EDIT]
         box = Gtk.Box()
-        image1 = Gtk.Image.new_from_icon_name(Gtk.STOCK_YES, Gtk.IconSize.MENU)
-        box.pack_start(image1, True, True, 0)
-        image2 = Gtk.Image.new_from_icon_name(Gtk.STOCK_NO, Gtk.IconSize.MENU)
-        box.pack_start(image2, True, True, 0)
+        for img in img_list:
+            image = Gtk.Image.new_from_icon_name(img, Gtk.IconSize.MENU)
+            box.pack_start(image, True, True, 0)
         self.add(box)
 
 
