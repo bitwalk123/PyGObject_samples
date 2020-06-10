df5d67011484235f2bf0b64c1b447781c76b65e0
Keiichi Takahashi <bitwalk@gmail.com>
2020-06-11 07:16:35 +0900

    add gtk_dialog_3.py for example of class inheritance

---
+++ gtk_dialog_3.py
@@ -0,0 +1,62 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class OKCancelDlg(Gtk.Dialog):
+
+    def __init__(self, parent, title):
+        Gtk.Dialog.__init__(self, parent=parent, title=title)
+        self.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        self.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+        self.set_default_size(0, 0)
+        self.set_resizable(False)
+        self.set_modal(True)
+
+
+class MyDialog(OKCancelDlg):
+
+    def __init__(self, parent):
+        OKCancelDlg.__init__(self, parent=parent, title="ダイアログ")
+
+        msg = Gtk.TextBuffer()
+        msg.set_text("このウィンドウは、追加情報を表示するためのダイアログです。")
+        tv = Gtk.TextView()
+        tv.set_wrap_mode(wrap_mode=Gtk.WrapMode.WORD)
+        tv.set_editable(False)
+        tv.set_buffer(msg)
+
+        content = self.get_content_area()
+        content.add(tv)
+
+        self.show_all()
+
+
+class MyWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="メイン")
+        self.set_default_size(0, 0)
+
+        but = Gtk.Button(label="ダイアログを開く")
+        but.connect("clicked", self.on_button_clicked)
+
+        self.add(but)
+
+    def on_button_clicked(self, widget):
+        dialog = MyDialog(self)
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            print("「OK」ボタンがクリックされました。")
+        elif response == Gtk.ResponseType.CANCEL:
+            print("「Cancel」ボタンがクリックされました。")
+
+        dialog.destroy()
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
