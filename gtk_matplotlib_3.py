b7cf64cbcd2be3b5be3b4589898bb633fc5ef11f
Fuhito Suguri <bitwalk@gmail.com>
2020-07-11 18:31:07 +0900

    update

--- gtk_matplotlib_3.py
+++ gtk_matplotlib_3.py
@@ -52,7 +52,7 @@ class MyWindow(Gtk.Window):
 
     def gen_plot_area(self, figure):
         sw = Gtk.ScrolledWindow()
-        sw.set_border_width(10)
+        sw.set_border_width(0)
         canvas = FigureCanvas(figure)  # a Gtk.DrawingArea
         canvas.set_size_request(800, 600)
         sw.add(canvas)
@@ -92,100 +92,50 @@ class MyWindow(Gtk.Window):
 
     def on_button_clicked(self, button, figure):
         image_path = "./chart.png"
-        figure.savefig(image_path)
-        self.gen_powerpoint(image_path)
-
-    def gen_powerpoint(self, image_path):
-        # ---------------------------------------------------
-        # "テンプレート、出力ファイル名の設定"
-        # ---------------------------------------------------
-        # templateとなるpptxファイルを指定する。
         template_path = "./template.pptx"
-        # 出力するpptxファイルを指定する。(存在しない場合、自動作成されます)
         save_path = "./output.pptx"
-        # ---------------------------------------------------
-        # "空のスライドの挿入"
-        # ---------------------------------------------------
+
+        # create PNG file of plots
+        figure.savefig(image_path)
+
+        # create PowerPoint files
+        self.gen_ppt(image_path, template_path, save_path)
+
+        # complete messages
+        dialog = Gtk.MessageDialog(parent=self,
+                                   flags=0,
+                                   message_type=Gtk.MessageType.INFO,
+                                   buttons=Gtk.ButtonsType.OK,
+                                   text="PowerPoint ファイル " + save_path + " を生成しました。")
+        dialog.run()
+        dialog.destroy()
+
+    def gen_ppt(self, image_path, template_path, save_path):
+        # insert empty slide
         presentation = Presentation(template_path)
-        title_slide_layout = presentation.slide_layouts[1]  # レイアウトや書式を元ファイルから参照する
+        # refer layout from original master
+        title_slide_layout = presentation.slide_layouts[4]
         slide = presentation.slides.add_slide(title_slide_layout)
         shapes = slide.shapes
-        # ---------------------------------------------------
-        # タイトルテキストの挿入"
-        # ---------------------------------------------------
-        # 入力したい文字列
-        slide_title = "TEST TITLE"
+        # slide title
+        slide_title = "SPC chart (Example)"
         shapes.title.text = slide_title
+
         # ---------------------------------------------------
-        # "imageの挿入"
+        # insert image
         # ---------------------------------------------------
-        # 挿入する位置
-        pic_left = self.Centis(1)
-        pic_top = self.Centis(5)
-        # imageの高さを指定
-        pic_height = self.Centis(7.9)
+        # insert position
+        pic_left = self.convVal(3)
+        pic_top = self.convVal(3)
+        # image height
+        pic_height = self.convVal(15)
         slide.shapes.add_picture(image_path, pic_left, pic_top, height=pic_height)
-        # ---------------------------------------------------
-        # "tableの挿入"
-        # ---------------------------------------------------
-        # 入力したいtable状のデータ
-        sample_table = [["1.1", "1.2", "1.3"]
-            , ["2.1", "2.2", "2.3"]
-            , ["3.1", "3.2", "3.3"]]
-        # cell内のフォントサイズ
-        cell_font = 20
-        # 挿入する位置
-        table_left = self.Centis(9.4)
-        table_top = self.Centis(5)
-        # tableの幅と高さ（仮）
-        table_width = self.Centis(15)
-        table_height = self.Centis(10)
-        # tableの行数と列数(tableのサイズ)
-        rows = len(sample_table)
-        cols = len(sample_table[0])
-        table = slide.shapes.add_table(rows, cols, table_left, table_top, table_width, table_height).table
-        # 表の各セルの中身を記入
-        for i in range(rows):
-            for j in range(cols):
-                cell = table.cell(i, j)
-                cell.text = sample_table[i][j]
-                cell.text_frame.paragraphs[0].font.size = Pt(cell_font)
-        # tableの高さを再調整
-        table.rows[0].height = self.Centis(1.5)
-        table.rows[1].height = self.Centis(4.9)
-        table.rows[2].height = self.Centis(1.5)
-        # tableの幅を再調整
-        table.columns[0].width = self.Centis((15) / 3)
-        table.columns[1].width = self.Centis((15) / 3)
-        table.columns[2].width = self.Centis((15) / 3)
-        # ---------------------------------------------------
-        # "テキストボックスの挿入"
-        # ---------------------------------------------------
-        # 文字列
-        sample_str = "Test Text"
-        # テキストボックスの位置
-        text_left = self.Centis(1)
-        text_top = self.Centis(13.4)
-        # テキストボックスの幅と高さ
-        text_width = self.Centis(25.4 - 2)
-        text_height = self.Centis(5)
-        # 文字のフォントサイズ
-        text_font = 20
-        # 塗りつぶし色指定(R, G, B)
-        color = RGBColor(150, 255, 255)
-        text_box = slide.shapes.add_textbox(text_left, text_top, text_width, text_height)
-        text_box.text = sample_str
-        text_box.text_frame.add_paragraph().font.size = Pt(text_font)
-        text_box.fill.solid()
-        text_box.fill.fore_color.rgb = color
-        # ---------------------------------------------------
-        # "ファイル保存"
-        # ---------------------------------------------------
+        # save PowerPoint Files
         presentation.save(save_path)
 
-    def Centis(self, length):
-        centi = Inches(length / 2.54)
-        return centi
+    def convVal(self, value):
+        # 1 inch = 2.54 cm
+        return Inches(value / 2.54)
 
 
 win = MyWindow()
