07976a4aa0cf456f0f04023eb1f597d9a94936fe
Keiichi Takahashi <bitwalk@gmail.com>
2022-05-03 07:47:26 +0900

    update to GTK4

--- gtk_linkbutton.py
+++ gtk_linkbutton.py
@@ -1,20 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
 import gi
 
-gi.require_version('Gtk', '3.0')
+gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 
-class MyWindow(Gtk.Window):
-
-    def __init__(self):
-        Gtk.Window.__init__(self, title="リンクボタン")
-        self.set_default_size(0, 0)
-
+class Example(Gtk.Window):
+    def __init__(self, app):
+        Gtk.Window.__init__(self, application=app, title="リンクボタン")
+        # LinkButton
         lb = Gtk.LinkButton(uri="https://pygobject.readthedocs.io/", label="PyGObject サイトへ")
-        self.add(lb)
+        self.set_child(lb)
 
 
-win = MyWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+class MyApplication(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id='com.blogspot.bitwalk')
+
+    def do_activate(self):
+        win = Example(self)
+        win.present()
+
+
+def main():
+    app = MyApplication()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
