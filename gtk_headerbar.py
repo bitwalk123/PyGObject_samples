a0309cba8ce947736a9224e856d4eeb1fbe5c8f0
Fuhito Suguri <bitwalk@gmail.com>
2023-12-05 06:22:03 +0900

    update

--- gtk_headerbar.py
+++ gtk_headerbar.py
@@ -21,58 +21,55 @@ class Example(Gtk.ApplicationWindow):
             # default_width=450
         )
 
-        action = Gio.SimpleAction.new('button1_clicked')
-        action.connect('activate', self.on_button1_clicked)
+        action = Gio.SimpleAction.new('button_left_clicked')
+        action.connect('activate', self.on_button_left_clicked)
         self.add_action(action)
 
-        action = Gio.SimpleAction.new('button2_clicked')
-        action.connect('activate', self.on_button2_clicked)
+        action = Gio.SimpleAction.new('button_right_clicked')
+        action.connect('activate', self.on_button_right_clicked)
         self.add_action(action)
 
         headerbar = Gtk.HeaderBar()
         self.set_titlebar(headerbar)
 
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-
-        button1 = Gtk.Button.new_from_icon_name('pan-start-symbolic')
-        button1.set_action_name('win.button1_clicked')
-        box.append(button1)
-
-        button2 = Gtk.Button.new_from_icon_name('pan-end-symbolic')
-        button2.set_action_name('win.button2_clicked')
-        box.append(button2)
-
         headerbar.pack_start(box)
 
-        # Gtk.HeaderBarに追加するGtk.Popoverに載せるものの作成
-        hamburger_menu = Gtk.Box(
+        button_left = Gtk.Button.new_from_icon_name('pan-start-symbolic')
+        button_left.set_action_name('win.button_left_clicked')
+        box.append(button_left)
+
+        button_right = Gtk.Button.new_from_icon_name('pan-end-symbolic')
+        button_right.set_action_name('win.button_right_clicked')
+        box.append(button_right)
+
+        hamburger_layout = Gtk.Box(
             orientation=Gtk.Orientation.VERTICAL,
             spacing=10
         )
-        hamburger_item_1 = Gtk.Button(
+        self.popover = Gtk.Popover(
+            position=Gtk.PositionType.BOTTOM,
+            child=hamburger_layout
+        )
+        hamburger_menubutton = Gtk.MenuButton(
+            popover=self.popover,
+            icon_name='open-menu-symbolic'
+        )
+        headerbar.pack_end(hamburger_menubutton)
+
+        hamburger_button_about = Gtk.Button(
             action_name='app.about',
             label='このアプリについて',
             has_frame=False,
             receives_default=True
         )
-        hamburger_menu.append(hamburger_item_1)
+        hamburger_layout.append(hamburger_button_about)
 
-        # Gtk.MenuButtonにGtk.Popoverを紐付け
-        self.popover = Gtk.Popover(
-            position=Gtk.PositionType.BOTTOM,
-            child=hamburger_menu
-        )
 
-        hamburger_button = Gtk.MenuButton(
-            popover=self.popover,
-            icon_name='open-menu-symbolic'
-        )
-        headerbar.pack_end(hamburger_button)
-
-    def on_button1_clicked(self, action, param):
+    def on_button_left_clicked(self, action, param):
         print('ボタン pan-start-symbolic が押されました。')
 
-    def on_button2_clicked(self, action, param):
+    def on_button_right_clicked(self, action, param):
         print('ボタン pan-end-symbolic が押されました。')
 
 
