18d5bead071b760ecce9a5c6d2d4edc513243628
Fuhito Suguri <bitwalk@gmail.com>
2023-12-05 11:57:24 +0900

    update

--- gtk_togglebutton.py
+++ gtk_togglebutton.py
@@ -1,41 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
 import gi
-
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 
+APPID = 'com.blogspot.bitwalk'
+
+
 class Example(Gtk.Window):
+
     def __init__(self, app):
-        Gtk.Window.__init__(self, application=app, title="トグルボタン")
-        # Box layout
+        Gtk.Window.__init__(
+            self,
+            application=app,
+            title='ToggleButton'
+        )
+
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.set_child(box)
-        #
-        cb1 = Gtk.ToggleButton(label="トグルボタンＡ")
-        cb1.connect("toggled", self.on_button_toggled, "Ａ")
+
+        cb1 = Gtk.ToggleButton(label='トグルボタンＡ')
+        cb1.connect('toggled', self.on_button_toggled, 'Ａ')
         box.append(cb1)
-        #
-        cb2 = Gtk.ToggleButton(label="トグルボタンＢ")
-        cb2.connect("toggled", self.on_button_toggled, "Ｂ")
+
+        cb2 = Gtk.ToggleButton(label='トグルボタンＢ')
+        cb2.connect('toggled', self.on_button_toggled, 'Ｂ')
         box.append(cb2)
-        #
-        cb3 = Gtk.ToggleButton(label="トグルボタンＣ")
-        cb3.connect("toggled", self.on_button_toggled, "Ｃ")
+
+        cb3 = Gtk.ToggleButton(label='トグルボタンＣ')
+        cb3.connect('toggled', self.on_button_toggled, 'Ｃ')
         box.append(cb3)
 
-    def on_button_toggled(self, button, name):
+    @staticmethod
+    def on_button_toggled(button, name):
         if button.get_active():
-            state = "オン"
+            state = 'オン'
         else:
-            state = "オフ"
-        print("トグルボタン%sは「%s」になりました。" % (name, state))
+            state = 'オフ'
+        print('トグルボタン%sは「%s」になりました。' % (name, state))
 
 
 class MyApplication(Gtk.Application):
+
     def __init__(self):
-        Gtk.Application.__init__(self, application_id='com.blogspot.bitwalk')
+        Gtk.Application.__init__(self, application_id=APPID)
 
     def do_activate(self):
         win = Example(self)
---
+++ gtk_togglebutton_gtk3.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+import gi
+
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
+
+
+class Example(Gtk.Window):
+    def __init__(self, app):
+        Gtk.Window.__init__(self, application=app, title="トグルボタン")
+        # Box layout
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(box)
+        #
+        cb1 = Gtk.ToggleButton(label="トグルボタンＡ")
+        cb1.connect("toggled", self.on_button_toggled, "Ａ")
+        box.append(cb1)
+        #
+        cb2 = Gtk.ToggleButton(label="トグルボタンＢ")
+        cb2.connect("toggled", self.on_button_toggled, "Ｂ")
+        box.append(cb2)
+        #
+        cb3 = Gtk.ToggleButton(label="トグルボタンＣ")
+        cb3.connect("toggled", self.on_button_toggled, "Ｃ")
+        box.append(cb3)
+
+    def on_button_toggled(self, button, name):
+        if button.get_active():
+            state = "オン"
+        else:
+            state = "オフ"
+        print("トグルボタン%sは「%s」になりました。" % (name, state))
+
+
+class MyApplication(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id='com.blogspot.bitwalk')
+
+    def do_activate(self):
+        win = Example(self)
+        win.present()
+
+
+def main():
+    app = MyApplication()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
